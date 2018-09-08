ffdc7513fee349665fc399ebc41272a54875dc46
aimwli <jin17.song@gmail.com>
2018-09-08 19:56:12 +0800

    模拟vx群发红包

---
+++ RedPacket/MyRedPacket/RedPacket.java
@@ -0,0 +1,14 @@
+package MyRedPacket;
+
+import known.RedPacketFrame;
+
+public class RedPacket extends RedPacketFrame {
+    /**
+     * 构造方法:生成红包界面.
+     *
+     * @param title 页面的标题.
+     */
+    public RedPacket(String title) {
+        super(title);
+    }
+}
