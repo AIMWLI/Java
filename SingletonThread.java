b5cd333f432c45f62a64d992f4970eeea4a48dd7
aimwli <jin17.song@gmail.com>
2019-04-02 10:29:32 +0800

    unit test

---
+++ SingletonThread.java
@@ -0,0 +1,22 @@
+package com.songj;
+
+import java.lang.Thread.sleep;
+
+public class SingletonThread {
+ 
+	public static void main(String[] args) {
+		Thread thread = Thread.currentThread();
+		thread.setName("单线程");
+		System.out.println(thread.getName()+"正在运行");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("线程正在休眠："+i);
+			try {
+				sleep(500);
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("线程出现错误！");
+			}
+		}
+		
+	}
+}
